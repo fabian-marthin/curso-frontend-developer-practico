0e7c597732780c9a2fa1940c8886833bea1ee87d
Favian Martinez <martthin74@gmail.com>
2022-11-29 12:44:21 -0500

    Se guardan fusiona index con la clase 13

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuEmail = document.querySelector(".navbar-email");
+const menuIcon = document.querySelector(".menu");
+const mobileMenuInactive = document.querySelector("mobile-menu");
 
 menuEmail.addEventListener("click", toggleMenu);
+menuIcon.addEventListener("click", toggleMenuMobile);
 
 function toggleMenu(){
     desktopMenu.classList.toggle("inactive");
+    
+}
+function toggleMenuMobile(){
+    mobileMenuInactive.classList.toggle("inactive");
 }
