87bfb5936546e880de26c2718734fa803dbb488c
Favian Martinez <martthin74@gmail.com>
2022-11-29 19:55:16 -0500

    se realiza cambios en js para las tarjetas de los produstos

--- main.js
+++ main.js
@@ -40,3 +40,53 @@ function menuOrden(){
 
     orden.classList.toggle("inactive");
 }
+
+const listaDeProductos = [];
+
+listaDeProductos.push({
+    nombre: "bike",
+    precio: 500000,
+    imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+listaDeProductos.push({
+    nombre: "TV",
+    precio: 1200000,
+    imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+listaDeProductos.push({
+    nombre: "juego de sala",
+    precio: 1500000,
+    imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+listaDeProductos.push({
+    nombre: "computador",
+    precio: 2000000,
+    imagen: "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+})
+
+/*<section class="main-container">
+    <div class="cards-container">
+
+      <div class="product-card">
+        <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+        <div class="product-info">
+          <div>
+            <p>$120,00</p>
+            <p>Bike</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+      </div>
+    </div>
+  </section>*/
+
+  for (product of listaDeProductos){
+    const tarjetaProducto = document.createElement("div");
+    tarjetaProducto.classList.add("product-card");
+
+    const img = document.createElement("img");
+    img.
+  }
+  
